b66575cf09a630b03ad19ba5034cf4e6fb6fb844
Raphael Ragoomundun <rragoomundun@protonmail.com>
2024-08-31 15:33:20 +0400

    [WIP] New Offer

--- src/app/modules/new-offer/components/new-offer/new-offer.component.ts
+++ src/app/modules/new-offer/components/new-offer/new-offer.component.ts
@@ -1,13 +1,70 @@
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { TranslateModule } from '@ngx-translate/core';
 
 import { ImageUploaderComponent } from '../../../../shared/components/image-uploader/image-uploader.component';
+import { SelectComponent } from '../../../../shared/components/select/select.component';
+import { InputComponent } from '../../../../shared/components/input/input.component';
+import { TextareaComponent } from '../../../../shared/components/textarea/textarea.component';
+
+import { ErrorUtil } from '../../../../shared/utils/error.util';
 
 @Component({
   selector: 'app-new-offer',
   standalone: true,
-  imports: [TranslateModule, ImageUploaderComponent],
+  imports: [
+    TranslateModule,
+    ReactiveFormsModule,
+    ImageUploaderComponent,
+    SelectComponent,
+    InputComponent,
+    TextareaComponent,
+  ],
   templateUrl: './new-offer.component.html',
   styleUrl: './new-offer.component.scss',
 })
-export class NewOfferComponent {}
+export class NewOfferComponent {
+  newOfferForm: FormGroup;
+
+  constructor() {
+    this.newOfferForm = new FormGroup({
+      typeOfGood: new FormControl('', [Validators.required]),
+      transactionType: new FormControl('', [Validators.required]),
+      squareMeters: new FormControl('', [Validators.required]),
+      rooms: new FormControl('', [Validators.required]),
+      bedrooms: new FormControl('', [Validators.required]),
+      price: new FormControl('', [Validators.required]),
+      city: new FormControl('', [Validators.required]),
+      description: new FormControl(''),
+    });
+  }
+
+  get squareMetersError(): string {
+    return '';
+  }
+
+  get roomsError(): string {
+    return '';
+  }
+
+  get bedroomsError(): string {
+    return '';
+  }
+
+  get priceError(): string {
+    return '';
+  }
+
+  get cityError(): string {
+    return '';
+  }
+
+  onShowFormValueClick(): void {
+    console.log(this.newOfferForm.value);
+  }
+}
