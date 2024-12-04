c7a3925cb40797aaa493ad252fb4fe5997caf684
Raphael Ragoomundun <rragoomundun@protonmail.com>
2024-12-04 11:02:02 +0400

    Updated environment, port number, and dist

--- server.ts
+++ server.ts
@@ -20,10 +20,13 @@ export function app(): express.Express {
   // Example Express Rest API endpoints
   // server.get('/api/**', (req, res) => { });
   // Serve static files from /browser
-  server.get('**', express.static(browserDistFolder, {
-    maxAge: '1y',
-    index: 'index.html',
-  }));
+  server.get(
+    '**',
+    express.static(browserDistFolder, {
+      maxAge: '1y',
+      index: 'index.html',
+    }),
+  );
 
   // All regular routes use the Angular engine
   server.get('**', (req, res, next) => {
@@ -45,7 +48,7 @@ export function app(): express.Express {
 }
 
 function run(): void {
-  const port = process.env['PORT'] || 4000;
+  const port = process.env['PORT'] || 4001;
 
   // Start up the Node server
   const server = app();
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,3 @@
 export const environment = {
-  API_URL: 'http://52.208.85.133:5001/v1',
+  API_URL: 'http://54.247.222.69:5001/v1',
 };
